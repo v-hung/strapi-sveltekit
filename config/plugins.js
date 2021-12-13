bae0b8c3f40d262b8c915ee2b2b70635db23f453
Việt Hùng <viet.hung.2898@gmail.com>
2021-12-13 18:18:03 +0700

    add

---
+++ config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ...
+  'variants': {
+    enabled: true,
+    resolve: './src/plugins/variants'
+  },
+  // ...
+}
---
+++ src/plugins/variants/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin variants
+
+A quick description of variants.
